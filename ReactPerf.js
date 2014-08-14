2b5d6592655be892fdead16250ad1f8ee1c1d9d1
Jason Brown <jbrown@jamasoftware.com>
2014-08-14 16:42:31 -0700

    Final fix that actually works

--- ReactPerf.js
+++ ReactPerf.js
@@ -5,8 +5,7 @@ if ( typeof module !== "undefined" && module.exports ) {
     module.exports = ReactPerf;
 }
 
-// ... or as AMD module
-else if ( typeof define === "function" && define.amd ) {
+if ( typeof define === "function" && define.amd ) {
     define( function () {
         return ReactPerf;
     });
